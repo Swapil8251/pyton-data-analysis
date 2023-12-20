d5109177c0394bbaa4f7c8cd1576712f215444df
Swapnil8251 <swapnilpatel41.0059@gmail.com>
2023-12-20 23:52:07 +0530

    age calculator

---
+++ all.py
@@ -0,0 +1,23 @@
+from datetime import date
+ 
+def calculateAge(born):
+    today = date.today()
+    try:
+        birthday = born.replace(year = today.year)
+    except ValueError:
+        birthday = born.replace(year = today.year,
+                  month = born.month + 1, day = 1)
+ 
+    if birthday > today:
+        return today.year - born.year - 1
+    else:
+        return today.year - born.year
+
+print(calculateAge(date(2000, 8, 10)), "years")
+
+
+
+
+
+
+
